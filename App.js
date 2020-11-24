6d09591caaa393b61f5098d6599e179b2cdbe66b
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-11-24 16:38:36 -0300

    stack navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as Font from "expo-font";
 import Home from "./screens/home";
 import { AppLoading } from "expo";
+import Navigator from "./routes/homeStack";
 
 const getFonts = () =>
   Font.loadAsync({
@@ -13,7 +14,7 @@ export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
 
   if (fontsLoaded) {
-    return <Home />;
+    return <Navigator />;
   } else {
     return (
       <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
