457bb8f7baefecea3ce05605f4d26e52679f2967
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-11-24 21:59:53 -0300

    navigation options

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -6,12 +6,26 @@ import ReviewDetails from "../screens/reviewDetails";
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: "GameZone",
+    },
   },
   ReviewDetails: {
     screen: ReviewDetails,
+    navigationOptions: {
+      title: "Review Details",
+    },
   },
 };
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: "#444",
+    headerStyle: {
+      backgroundColor: "#eee",
+      height: 60,
+    },
+  },
+});
 
 export default createAppContainer(HomeStack);
