3cf264ce71c465b085a1558399e9f4b6d87a5c52
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-04 15:30:58 -0300

    created a modal to insert the future form

--- screens/home.js
+++ screens/home.js
@@ -1,31 +1,40 @@
 import React, { useState } from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, Modal, StyleSheet } from "react-native";
 import { globalStyles } from "../styles/global";
 import Card from "../shared/card";
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Home({ navigation }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const [reviews, setReviews] = useState([
-    {
-      title: "Zelda, Breath of Fresh Air",
-      rating: 5,
-      body: "lorem ipsum",
-      key: 1,
-    },
-    {
-      title: "Gotta Catch Them All (again)",
-      rating: 4,
-      body: "lorem ipsum",
-      key: 2,
-    },
-    {
-      title: "Not So 'Final' Fantasy",
-      rating: 3,
-      body: "lorem ipsum",
-      key: 3,
-    },
+    { title: "Zelda, Breath of Fresh Air", rating: 5, body: "lorem ipsum", key: 1 },
+    { title: "Gotta Catch Them All (again)", rating: 4, body: "lorem ipsum", key: 2 },
+    { title: "Not So 'Final' Fantasy", rating: 3, body: "lorem ipsum", key: 3 },
   ]);
+
   return (
     <View style={globalStyles.container}>
+
+      <Modal visible={modalOpen} animationType="slide">
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            style={{ ...styles.modalToggle, ...styles.modalClose }}
+            onPress={() => setModalOpen(false)}
+          />
+          <Text>Hello from the modal :)</Text>
+        </View>
+      </Modal>
+
+      <MaterialIcons
+        name="add"
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
+
       <FlatList
         data={reviews}
         renderItem={({ item }) => (
@@ -41,3 +50,21 @@ export default function Home({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalToggle: {
+    marginHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#F2F2F2",
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: {
+    flex: 1,
+  }
+})
