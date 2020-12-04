c14b075d8bf3ab9aed4574cdfec462fd709815da
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-04 18:54:07 -0300

    form component created

---
+++ screens/reviewForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
+import { globalStyles } from '../styles/global';
+import { Formik } from 'formik';
+
+export default function RevireForm() {
+  return (
+    <View style={globalStyles.container}>
+      <Formik
+        initialValues={{ title: "", body: "", rating: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(props) => (
+          <View>
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Review title"
+              onChangeText={props.handleChange("title")}
+              value={props.values.title}
+            />
+
+            <TextInput
+              multiline
+              style={globalStyles.input}
+              placeholder="Review body"
+              onChangeText={props.handleChange("body")}
+              value={props.values.body}
+            />
+
+            <TextInput
+              style={globalStyles.input}
+              placeholder="Rating (1-5)"
+              onChangeText={props.handleChange("rating")}
+              value={props.values.rating}
+              keyboardType="numeric"
+            />
+
+            <Button title="submit" color="maroon" onPress={props.handleSubmit} />
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
+};
