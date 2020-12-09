bc1c1ccf1823018e846c4dc87e8ffa9ed81e3d55
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-12-09 19:32:24 -0300

    custom button component

--- screens/home.js
+++ screens/home.js
@@ -7,20 +7,29 @@ import {
   Modal,
   StyleSheet,
   TouchableWithoutFeedback,
-  Keyboard
+  Keyboard,
 } from "react-native";
 import { globalStyles } from "../styles/global";
 import Card from "../shared/card";
 import { MaterialIcons } from "@expo/vector-icons";
 import ReviewForm from "./reviewForm";
 
-
 export default function Home({ navigation }) {
   const [modalOpen, setModalOpen] = useState(false);
 
   const [reviews, setReviews] = useState([
-    { title: "Zelda, Breath of Fresh Air", rating: 5, body: "lorem ipsum", key: 1 },
-    { title: "Gotta Catch Them All (again)", rating: 4, body: "lorem ipsum", key: 2 },
+    {
+      title: "Zelda, Breath of Fresh Air",
+      rating: 5,
+      body: "lorem ipsum",
+      key: 1,
+    },
+    {
+      title: "Gotta Catch Them All (again)",
+      rating: 4,
+      body: "lorem ipsum",
+      key: 2,
+    },
     { title: "Not So 'Final' Fantasy", rating: 3, body: "lorem ipsum", key: 3 },
   ]);
 
@@ -30,11 +39,10 @@ export default function Home({ navigation }) {
       return [review, ...currentReviews];
     });
     setModalOpen(false);
-  }
+  };
 
   return (
     <View style={globalStyles.container}>
-
       <Modal visible={modalOpen} animationType="slide">
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <View style={styles.modalContent}>
@@ -60,6 +68,7 @@ export default function Home({ navigation }) {
         data={reviews}
         renderItem={({ item }) => (
           <TouchableOpacity
+            key={item.key}
             onPress={() => navigation.navigate("ReviewDetails", item)}
           >
             <Card>
@@ -87,5 +96,5 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     flex: 1,
-  }
-})
+  },
+});
--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { StyleSheet, Button, TextInput, View, Text } from "react-native";
+import { Button, TextInput, View, Text } from "react-native";
 import { globalStyles } from "../styles/global";
 import { Formik } from "formik";
 import * as yup from "yup";
+import FlatButton from "../shared/button";
 
 const reviewSchema = yup.object({
   title: yup.string().required().min(4),
@@ -63,11 +64,7 @@ export default function RevireForm({ addReview }) {
               {props.touched.rating && props.errors.rating}
             </Text>
 
-            <Button
-              title="submit"
-              color="maroon"
-              onPress={props.handleSubmit}
-            />
+            <FlatButton text="submit" onPress={props.handleSubmit} />
           </View>
         )}
       </Formik>
